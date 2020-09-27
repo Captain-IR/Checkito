b65f169324fc4cbe7c7498d39a1b61a1dd12e831
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-09-27 18:56:09 +0200

    Added icons, electron & cordova sources

---
+++ src-electron/main-process/electron-main-menu-template.js
@@ -0,0 +1,107 @@
+const isMac = process.platform === "darwin";
+import { mainWindow } from "./electron-main";
+
+export const menuTemplate = [
+  // { role: 'appMenu' }
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            { role: "about" },
+            { type: "separator" },
+            { role: "services" },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideothers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" }
+          ]
+        }
+      ]
+    : []),
+  // { role: 'fileMenu' }
+  {
+    label: "File",
+    submenu: [
+      isMac
+        ? { role: "close" }
+        : {
+            label: "Settings",
+            accelerator: "CmdOrCtrl+,",
+            click: function() {
+              mainWindow.webContents.send("show-settings");
+            }
+          },
+      { role: "quit" }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: "Edit",
+    submenu: [
+      { role: "undo" },
+      { role: "redo" },
+      { type: "separator" },
+      { role: "cut" },
+      { role: "copy" },
+      { role: "paste" },
+      ...(isMac
+        ? [
+            { role: "pasteAndMatchStyle" },
+            { role: "delete" },
+            { role: "selectAll" },
+            { type: "separator" },
+            {
+              label: "Speech",
+              submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }]
+            }
+          ]
+        : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      { role: "forcereload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "resetzoom" },
+      { role: "zoomin" },
+      { role: "zoomout" },
+      { type: "separator" },
+      { role: "togglefullscreen" }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: "Window",
+    submenu: [
+      { role: "minimize" },
+      { role: "zoom" },
+      ...(isMac
+        ? [
+            { type: "separator" },
+            { role: "front" },
+            { type: "separator" },
+            { role: "window" }
+          ]
+        : [{ role: "close" }])
+    ]
+  },
+  {
+    role: "help",
+    submenu: [
+      {
+        label: "Learn More",
+        click: async () => {
+          const { shell } = require("electron");
+          await shell.openExternal("https://electronjs.org");
+        }
+      }
+    ]
+  }
+];
---
+++ src-electron/main-process/electron-main.js
@@ -0,0 +1,61 @@
+import { app, BrowserWindow, nativeTheme, Menu } from "electron";
+import { menuTemplate } from "./electron-main-menu-template";
+
+try {
+  if (
+    process.platform === "win32" &&
+    nativeTheme.shouldUseDarkColors === true
+  ) {
+    require("fs").unlinkSync(
+      require("path").join(app.getPath("userData"), "DevTools Extensions")
+    );
+  }
+} catch (_) {}
+
+/**
+ * Set `__statics` path to static files in production;
+ * The reason we are setting it here is that the path needs to be evaluated at runtime
+ */
+if (process.env.PROD) {
+  global.__statics = __dirname;
+}
+
+// variables & constants
+export let mainWindow;
+const menu = Menu.buildFromTemplate(menuTemplate);
+
+// app ready
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    minWidth: 800,
+    minHeight: 500,
+    useContentSize: true,
+    webPreferences: {
+      nodeIntegration: process.env.QUASAR_NODE_INTEGRATION,
+      nodeIntegrationInWorker: process.env.QUASAR_NODE_INTEGRATION
+    }
+  });
+
+  mainWindow.loadURL(process.env.APP_URL);
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  Menu.setApplicationMenu(menu);
+});
+
+// app events
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// app.on('activate', () => {
+//   if (mainWindow === null) {
+//     createWindow()
+//   }
+// })
